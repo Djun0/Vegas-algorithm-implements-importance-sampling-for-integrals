81a8828c0c2a330f3bf5023a72c608ad9262c511
Djun0 <73646479+Djun0@users.noreply.github.com>
2024-09-19 21:39:21 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,33 @@
-#include <vector>
-#include <iostream>
-#include <iomanip>
 
-#include "Vegas.cpp"
-double torusfunc(std::vector<double> const &x) {
-	//return 1 / (pow(x[0] - 0.5, 2) + 1e-12) + 1 / ((pow(x[1] - 0.7, 2) + 1e-12) * (pow(x[2] - 0.1, 2) + 1e-12) * (pow(x[0] - 0.1, 2) + 1e-12));
-	
-	double S = 0.,P=1.;
-	for (int i = 4; i <=19; i++) {
-		S = S + x[i];
+#include <vector>
+#include "vegas.h"
+double torusfunc(std::vector<double> const x, const double wgt) {
+	/*
+	double SUM = 0, P = 1;
+	for (int i = 4; i <= 19; i++) {
+		SUM += x[i];
 	}
 	for (int i = 20; i <= 29; i++) {
-		P= P* x[i];
+		P*= x[i];
 	}
-	return 4 * x[0] * x[2] * x[2] * exp(2 * x[0] * x[2]) * exp(S) * P / ((1 + x[1] + x[3]) * (1 + x[1] + x[3]));
-	//return x[1] * x[1] / (pow(x[0] - 0.5, 2) + 1e-12) + x[0] * x[0] / (pow(x[1] - 0.5, 2) + 1e-12);
+	return 4*x[0]*x[2]*x[2]*exp(2*x[0]*x[2]) * exp(SUM) * P / pow((1 + x[1] + x[3]),2);
+	*/
+	//return 1 / (pow(x[0] - 0.5, 2) + 1e-12) + 1 / ((pow(x[1] - 0.7, 2) + 1e-12) * (pow(x[2] - 0.1, 2) + 1e-12) * (pow(x[0] - 0.7, 2) + 1e-12));
+	return 1 / (pow(x[0] + x[1] - 0.5, 2) + 1e-12);
 }
-
-
 int main() {
+	int const dim = 2;
+	double tgral, sd, chi2a;
+	std::vector<double> regn(dim*2);
+	for (int i = 0; i < dim*2; i++) {
+		if( i< dim) regn[i] = 0.;else regn[i] = 1.;
+	}
+
+	vegas(regn, torusfunc, 0, 1000*1000, 10, 0, tgral, sd, chi2a);
+	vegas(regn, torusfunc, 1, 1000*1000, 10, 0, tgral, sd, chi2a);
+	vegas(regn, torusfunc, 2, 1000*1000, 5, 0, tgral, sd, chi2a);
 	
-	std::vector<double> lb(30,0);
-	std::vector<double> ub(30,1);
-	VEGASResult<double, double, double, std::vector<std::vector<double>>, std::vector<std::vector<double>>>
-		A = Vegas(torusfunc, lb, ub, 20, 1000,10000, 1e-4, 1e-4, 0.5, 10,20);
-	std::cout << "-----------------------------------------\n";
-	std::cout << "Uoc tinh tich luy cuoi cung\n";
-	std::cout << std::setw(35) << std::fixed << std::setprecision(5) << std::right << A.integral_estimate << std::endl;
 
-	std::cout << "Sai so\n";
-	std::cout << std::setw(35) << std::fixed << std::setprecision(5) << std::right << A.standard_deviation << std::endl;
 
-	std::cout << "Chi binh/iter-1\n";
-	std::cout << std::setw(35) << std::fixed << std::setprecision(5) << std::right << A.chi_squared_average << std::endl;
-
+	return 0;
 }
